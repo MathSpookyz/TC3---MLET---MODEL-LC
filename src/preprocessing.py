27b3e9a65d4edc2e4bee8a854b0912304317dd0c
Andre <andresnu@tutanota.com>
2025-09-30 22:00:05 -0300

    Kmean working

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,18 +1,18 @@
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
 from sklearn.compose import ColumnTransformer
 import pandas as pd
 
 def create_preprocessor(num_cols):
     preprocessor = ColumnTransformer([
-        ('num', StandardScaler(), num_cols),
+        ('num', MinMaxScaler(), num_cols),
     ])
     return preprocessor
 
 def preview_transformation(preprocessor, df, n=5):
     """Mostra resultado do preprocessor para as n primeiras linhas."""
     transformed = preprocessor.fit_transform(df)
-    transformed_df = pd.DataFrame(transformed)
-    print("\nShape original:", df.shape)
-    print("Shape transformado:", transformed_df.shape)
-    print(transformed_df.head(n))
-    return transformed_df
+    # transformed_df = pd.DataFrame(transformed)
+    # print("\nShape original:", df.shape)
+    # print("Shape transformado:", transformed_df.shape)
+    # print(transformed_df.head(n))
+    return transformed
